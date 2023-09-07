c13e2befc8fe4bd39103b8be94c3c407a19451d6
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-07 19:34:29 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* main- Entry point of program
+*
+* Return: 0 if program is executed successfully
+*/
+int main(void)
+{	
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (2, quote, strlen(quote));
+	return (0);
+}
