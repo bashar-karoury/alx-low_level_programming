15b20670bc3f0696272eb0f749bc214fa9f101dc
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-07 16:50:08 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main(){
+/**
+* main- Entry point of program
+*
+* Return: 0 if program is executed successfully
+*/
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
