0633e4580140b913176b993befccb52fa51644cf
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-08 08:58:30 +0300

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point of Program
+*
+* Return: exit code of program
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
