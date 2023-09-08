42af31ae58dbfce66fca542502da85b99ba00230
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-08 08:52:50 +0300

    Update 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point of Program
+*
+* Return: exit code of program
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
