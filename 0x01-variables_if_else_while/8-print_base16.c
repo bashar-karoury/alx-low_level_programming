6d06cf3697ed93cd1f834b90c24b1701b21f7c48
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-08 09:08:29 +0300

    Add Script 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point of Program
+*
+* Return: exit code of program
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
