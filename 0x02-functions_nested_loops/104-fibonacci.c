beb20a596d574fbc93ae2b96653d50b2fb4eec02
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-12 21:45:28 +0300

    Add 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/**
+* main - program that print the first fibonacci number
+*
+* Return: 0 on Success
+*/
+int main(void)
+{
+
+	int count = 2;
+	long int pre_number = 2;
+	long int pre_pre_number = 1;
+	long int printed_number;
+
+	printf("1, ");
+	printf("2, ");
+	while (count <= 49)
+	{
+		printed_number = pre_number + pre_pre_number;
+		printf("%ld", printed_number);
+		if (count < 49)
+		{
+			printf(", ");
+		}
+		pre_pre_number = pre_number;
+		pre_number = printed_number;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
