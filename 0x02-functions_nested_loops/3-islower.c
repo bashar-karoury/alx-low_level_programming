0cca45ec4d98462d99ff25740105fbe2cdbf76e3
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-12 12:01:58 +0300

    Add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _islower - function that check if passed character is lower or not
+* @c: The character to be tested
+*
+* Return: 1 if @c is lower, otherwise it returns 0
+ */
+int _islower(int c)
+{
+	return ((c >= 79) ? 1 : 0);
+}
