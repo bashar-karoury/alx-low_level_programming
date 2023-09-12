8248b7b6ad737711a88c5ff4550eeb2fe52eaed6
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-12 18:59:59 +0300

    Add 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,15 @@
 * Return: Always 0
  */
 int main(void)
-{	
+{
 	int sum = 0;
 	int i;
 
-	for(i = 1; i < 1024; i++)
+	for (i = 1; i < 1024; i++)
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0))
 			sum += i;
 	}
-	printf("Sum = %d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
