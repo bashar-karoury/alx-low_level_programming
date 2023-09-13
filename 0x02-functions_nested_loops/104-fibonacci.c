2ce2cada5210dc5e747bba06dff695ebfeaa2e01
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-13 10:14:35 +0300

    Fix 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,37 +8,26 @@
 */
 int main(void)
 {
-
 	int count = 2;
 	unsigned long int pre_number = 2;
 	unsigned long int pre_pre_number = 1;
-	unsigned long int printed_number;
-	unsigned long int pre_number_right_h;
-	unsigned long int pre_number_left_h;
-	unsigned long int pre_pre_right_h;
-	unsigned long int pre_pre_left_h;
-	unsigned long int number_right_h;
+	unsigned long int printed_number, pre_number_right_h, pre_number_left_h;
+	unsigned long int pre_pre_right_h, pre_pre_left_h, number_right_h;
 	unsigned long int number_left_h;
 
-	printf("1, ");
-	printf("2, ");
-	while (count < 92)
+	printf("1, 2, ");
+	while (count++ < 92)
 	{
 		printed_number = pre_number + pre_pre_number;
-		printf("%lu", printed_number);
-		printf(", ");
-
+		printf("%lu, ", printed_number);
 		pre_pre_number = pre_number;
 		pre_number = printed_number;
-		count++;
 	}
-
 	pre_number_right_h = pre_number  % 1000000000;
 	pre_number_left_h = pre_number   / 1000000000;
 	pre_pre_right_h = pre_pre_number % 1000000000;
 	pre_pre_left_h = pre_pre_number  / 1000000000;
-
-	while (count <= 97)
+	while (count++ <= 97)
 	{
 		number_right_h = pre_number_right_h + pre_pre_right_h;
 		number_left_h = pre_number_left_h + pre_pre_left_h;
@@ -47,17 +36,13 @@ int main(void)
 			number_left_h++;
 			number_right_h = number_right_h % 1000000000;
 		}
-
 		printf("%lu%lu", number_left_h, number_right_h);
 		if (count < 97)
-		{
 			printf(", ");
-		}
 		pre_pre_right_h = pre_number_right_h;
 		pre_pre_left_h = pre_number_left_h;
 		pre_number_right_h = number_right_h;
 		pre_number_left_h = number_left_h;
-		count++;
 	}
 	printf("\n");
 	return (0);
