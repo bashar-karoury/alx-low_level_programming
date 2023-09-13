1a14b51af9603a76aa04f63e75ef6c212aedf631
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-13 09:52:27 +0300

    Fix 104-fibonacci.c for large than long numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,20 +12,48 @@ int main(void)
 	int count = 2;
 	unsigned long int pre_number = 2;
 	unsigned long int pre_pre_number = 1;
-	unsigned long int  printed_number;
-
+	unsigned long int printed_number;
+	unsigned long int pre_number_right_h, pre_number_left_h, pre_pre_right_h, pre_pre_left_h, number_right_h, number_left_h;
 	printf("1, ");
 	printf("2, ");
-	while (count <= 97)
+	while (count < 92)
 	{
 		printed_number = pre_number + pre_pre_number;
 		printf("%lu", printed_number);
+		printf(", ");
+
+		pre_pre_number = pre_number;
+		pre_number = printed_number;
+		count++;
+	}
+	
+
+	pre_number_right_h = pre_number  % 1000000000;
+	pre_number_left_h = pre_number   / 1000000000;
+	pre_pre_right_h = pre_pre_number % 1000000000;
+	pre_pre_left_h = pre_pre_number  / 1000000000;
+
+	while (count <= 97)
+	{	
+		
+		number_right_h = pre_number_right_h + pre_pre_right_h;
+		number_left_h = pre_number_left_h + pre_pre_left_h;
+		if (number_right_h > 1000000000)
+		{
+		    	number_left_h++;
+			number_right_h = number_right_h % 1000000000;
+		}
+
+
+		printf("%lu%lu", number_left_h, number_right_h);
 		if (count < 97)
 		{
 			printf(", ");
 		}
-		pre_pre_number = pre_number;
-		pre_number = printed_number;
+		pre_pre_right_h = pre_number_right_h;
+		pre_pre_left_h = pre_number_left_h;
+		pre_number_right_h = number_right_h;
+		pre_number_left_h = number_left_h;
 		count++;
 	}
 	printf("\n");
