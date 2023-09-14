c470b40f12eb22934bd15463e45c9074668a2605
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-14 12:19:19 +0300

    Edit main.h

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,48 +1,28 @@
 #include "main.h"
 
-void printInt(int n);
+void print2DInt(int n);
 /**
 * more_numbers - print 10 times the numbers from 0 to 14
 */
 void more_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 14; i++)
 	{
-		printInt(i);
+		print2DInt(i);
 	}
 	_putchar('\n');
 }
 
 
 /**
-   * printInt - prints n to stdout using _putchar
+   * print2DInt - prints integers of 2 digits  to stdout using _putchar
     * @n: integer to be printed
      */
-void printInt(int n)
+void print2DInt(int n)
 {
-	int a[5] = {0};
-	int count = 0;
-	int i;
-
-	if (n == 0)
-	{
-		_putchar('0');
-	return;
-	}
-	if (n < 0)
-	{
-		n = -(n);
-		_putchar('-');
-	}
-	while (n)
-	{
-		a[count++] = n % 10;
-		n /= 10;
-	}
-
-	for (i = 0; i < count; i++)
-	{
-		_putchar('0' + a[count - i - 1]);
-	}
+	if (n >= 10)
+		_putchar('0' + (n / 10));
+	_putchar('0' + (n % 10));
 }
