b62d350805d26c21eadd98853ec9d2abfae53579
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-20 18:31:58 +0300

    Fix 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,16 +8,20 @@
 char *cap_string(char *s)
 {
 	int idx = 0;
+	int check = 0;
 
 	for (idx = 1; s[idx] != '\0'; idx++)
 	{
-		if ((s[idx - 1] == ',') || (s[idx - 1] == ';') ||\
-				(s[idx - 1] == '.') || (s[idx - 1] == '!') ||\
-				(s[idx - 1] == '\?') || (s[idx - 1] == '\"') ||\
-				(s[idx - 1] == '(') || (s[idx - 1] == ')') ||\
-				(s[idx - 1] == '{') || (s[idx - 1] == '}') ||\
-				(s[idx - 1] == '\n') || (s[idx - 1] == '\t') ||\
-				(s[idx - 1] == ' '))
+		check = 0;
+		check = check || (s[idx - 1] == ',') || (s[idx - 1] == ';');
+		check = check || (s[idx - 1] == '\"') || (s[idx - 1] == '(');
+		check = check || (s[idx - 1] == '\n') || (s[idx - 1] == '\t');
+		check = check || (s[idx - 1] == '!') || (s[idx - 1] == '\?');
+		check = check || (s[idx - 1] == '{') || (s[idx - 1] == '}');
+		check = check || (s[idx - 1] == '.') || (s[idx - 1] == ')');
+		check = check || (s[idx - 1] == ' ');
+
+		if (check)
 		{
 			if (s[idx] >= 'a' && s[idx] <= 'z')
 			{
