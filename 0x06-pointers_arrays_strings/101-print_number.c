cc231c48214046cc276e4f2f6308e05258891522
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-21 11:37:25 +0300

    Add 101-print_number.c main.h

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  * print_number - prints an integer using _putchar only
+  * @n: integer to be printed
+  */
+void print_number(int n)
+{
+/*
+* determine loop counter by divide n by 10
+*/
+	int temp = n;
+	int counter = 0;
+	int mul = 1;
+	int i;
+	int p;
+
+	if (n < 0)
+	{
+		n = -(n);
+		_putchar('-');
+	}
+	while (temp / 10 != 0)
+	{
+		temp /= 10;
+		counter++;
+		mul *= 10;
+	}
+
+
+	temp = n;
+	for (i = counter; i >= 0; i--)
+	{
+		p = temp / mul;
+		_putchar('0' + p);
+		temp = temp % (mul);
+		mul /= 10;
+	}
+
+}
