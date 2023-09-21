5a87c7b6df5bdbcfc9f1b9618595fd53214a5600
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-21 20:02:46 +0300

    FixBetty 104-print_buffer.c main.h

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,10 +8,10 @@
 */
 void print_buffer(char *b, int size)
 {
-
 	int j;
 	unsigned int rem = (10 - (size % 10));
 	unsigned int _size = size + rem;
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -20,48 +20,30 @@ void print_buffer(char *b, int size)
 	else
 	{
 		unsigned int i = 0;
-		
 
 		for (i = 0; i < _size; i++)
 		{
 			if (!(i % 10))
-			{
-				printf("%08x:",i);
-			}
-			
-			if(!(i%2))
-			{
+				printf("%08x:", i);
+			if (!(i % 2))
 				printf(" ");
-			}
-
-			if(i < (unsigned int)size)
-			{
+			if (i < (unsigned int)size)
 				printf("%02x", b[i]);
-			}
 			else
-			{
 				printf("  ");
-			}
-
 			if (!((i + 1) % 10) && (i != 0))
-			{	
+			{
 				printf(" ");
-				for(j = 0; j <10; j++)
-				{	
-					if (b[(i- 9) +j] < 0x20) 
-					{
+				for (j = 0; j < 10; j++)
+				{
+					if (b[(i - 9) + j] < 0x20)
 						printf(".");
-					}
 					else
-					{
-						printf("%c", b[(i- 9) +j]);
-					}
+						printf("%c", b[(i - 9) + j]);
 				}
 				printf("\n");
 			}
-
 		}
-
 		return;
 	}
 }
