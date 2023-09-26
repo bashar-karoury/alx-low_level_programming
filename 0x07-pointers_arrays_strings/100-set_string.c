b2ccaa7324a32a9fbb6eefddf746353f8e09fd6b
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-26 09:22:09 +0300

    Add 100-set_string.c main.h

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* set_string - set a value of pinter to char to another
+* @s: double pointer to char
+* @to: char pointer
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
