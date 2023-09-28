15a66d1efc5cdc0037d70e9f0e10d409ff890099
bashar-karoury <basharalkaroury@hotmail.com>
2023-09-29 01:15:32 +0300

    Add 6-is_prime_number.c main.h

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+int check_reminders(int n, int i);
+/**
+* is_prime_number - test if number is prime number or not
+* @n: number as integer
+*
+* Return: 1 if number is prime, otherwise 0
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1) 
+	{
+		return (0);
+	}
+	else if (n == 3 || n == 2)
+	{
+		return (1);
+	}
+
+	return check_reminders(n, n - 1);
+
+}
+
+int check_reminders(int n, int i)	
+{
+	int check = 0;
+	if (i == 1)
+		return 1;
+
+	check = (n % i) == 0 ? 0 : 1;
+	return (check & check_reminders(n, i-1));
+}
