56f22694ea672f3153708deede18c082c23dcc3f
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-10 11:51:33 +0300

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - struct that represetn dogs
+* @name: char pointer to string as dog name
+* @age: float as dog age
+* @owner: char pointer to strint as owner name
+*/
+struct dog
+{
+	char	*name;
+	float	age;
+	char	*owner;
+};
+
+#endif
