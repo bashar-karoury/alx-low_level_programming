b41b44cbfafc8f87fa7e6ba66502391ab7b82be3
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-19 21:34:11 +0300

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void pre_main(void) __attribute__((constructor));
+
+/**
+* pre_main - function to be called before main
+*/
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
