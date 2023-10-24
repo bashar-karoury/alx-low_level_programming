9a99150702b50a744fc17108d40fd0d15cbc0952
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-24 19:51:55 +0300

    Fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *next_node = NULL;
 
-	if (head != NULL)
+	if ((head != NULL) && (*head != NULL))
 	{
 		if (idx == 0)
 		{
