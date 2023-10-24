62df496377d9938129214d012f543beea092c5a4
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-24 20:18:35 +0300

    Add delete At index function

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	int result = 1;
 	listint_t *trav_node = NULL;
-	int i = 0;
+	unsigned int i = 0;
 	listint_t *pre_node = NULL;
 	listint_t *post_node = NULL;
 
