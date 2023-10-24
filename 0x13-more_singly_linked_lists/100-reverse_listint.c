854b8f33b0f768e098b444c1b957883c1b38c15e
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-25 00:22:13 +0300

    Fix reverse listint

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,10 +12,12 @@ listint_t *reverse(listint_t *node, listint_t **head);
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *Newhead = NULL;
-
-	reverse(*head, &Newhead);
-	(*head)->next = NULL;
-	*head = Newhead;
+	if (head != NULL && *head != NULL)
+	{
+		reverse(*head, &Newhead);
+		(*head)->next = NULL;
+		*head = Newhead;
+	}
 	return (Newhead);
 
 }
