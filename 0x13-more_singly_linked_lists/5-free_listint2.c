af28ada62fd32a1715a75b09d041def842f80496
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-24 17:03:11 +0300

    fix 5-free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,17 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *traverse_node = *head;
-	listint_t *temp_node = *head;
-
-	while (traverse_node != NULL)
+	if (*head != NULL)
 	{
-		temp_node = traverse_node;
-		traverse_node = traverse_node->next;
-		free(temp_node);
+		listint_t *traverse_node = *head;
+		listint_t *temp_node = *head;
+
+		while (traverse_node != NULL)
+		{
+			temp_node = traverse_node;
+			traverse_node = traverse_node->next;
+			free(temp_node);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
