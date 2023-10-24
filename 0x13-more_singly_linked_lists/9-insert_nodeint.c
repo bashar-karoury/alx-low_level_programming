b1fee9cee688b43a655063b3b9a08ad4d9000172
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-24 19:38:45 +0300

    Fix 9-insert_node_int

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
-* insert_nodeint_at_index - inserts new node at given position
-* @head: double pointer to head node
-* @idx: position at which the node should be inserted
-* @n: data of new node
-*
-* Return: pointer to the new inserted node
-*/
+ * insert_nodeint_at_index - inserts new node at given position
+ * @head: double pointer to head node
+ * @idx: position at which the node should be inserted
+ * @n: data of new node
+ *
+ * Return: pointer to the new inserted node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *result_node = NULL;
@@ -18,21 +18,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head != NULL)
 	{
-		trav_node = *head;
-		while ((trav_node != NULL) && (i < (idx - 1)))
+		if (idx == 0)
 		{
-			trav_node = trav_node->next;
-			i++;
-		}
-		if ((trav_node != NULL) && (i == (idx - 1)))
-		{
-			next_node = trav_node->next;
 			result_node = (listint_t *)malloc(sizeof(listint_t));
 			if (result_node)
 			{
+				if (*head == NULL)
+					result_node->next = NULL;
+				else
+					result_node->next = *head;
 				result_node->n = n;
-				trav_node->next = result_node;
-				result_node->next = next_node;
+				*head = result_node;
+			}
+		}
+		else
+		{
+			trav_node = *head;
+			while ((trav_node != NULL) && (i++ < (idx - 1)))
+				trav_node = trav_node->next;
+			if ((i == (idx - 1)))
+			{
+				next_node = trav_node->next;
+				result_node = (listint_t *)malloc(sizeof(listint_t));
+				if (result_node)
+				{
+					result_node->n = n;
+					trav_node->next = result_node;
+					result_node->next = next_node;
+				}
 			}
 		}
 	}
