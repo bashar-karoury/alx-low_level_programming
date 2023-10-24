1eb117617e5554c5f9d9ffb8665dce34e805a296
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-24 21:18:28 +0300

    Fix 10-delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,28 +11,13 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	int result = 1;
-	listint_t *trav_node = NULL;
+	listint_t *trav_node = NULL, *pre_node = NULL, *post_node = NULL;
 	unsigned int i = 0;
-	listint_t *pre_node = NULL;
-	listint_t *post_node = NULL;
 
-	if (head != NULL)
+	if ((head != NULL) && (*head != NULL))
 	{
 		trav_node = *head;
-
-		while ((trav_node != NULL) && (i++ < (index - 1)))
-			trav_node = trav_node->next;
-		if (i == (index - 1))
-		{
-			pre_node = trav_node;
-
-			trav_node = trav_node->next;
-			post_node = trav_node->next;
-
-			free(trav_node);
-			pre_node->next = post_node;
-		}
-		else if (index == 0)
+		if (index == 0)
 		{
 			post_node = trav_node->next;
 			free(trav_node);
@@ -40,9 +25,25 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		else
 		{
-			result = -1;
-		}
+			while ((trav_node != NULL) && (i < (index - 1)))
+			{
+				trav_node = trav_node->next;
+				i++;
+			}
+			if ((i == (index - 1)) && (trav_node->next != NULL))
+			{
+				pre_node = trav_node;
+				trav_node = trav_node->next;
+				post_node = trav_node->next;
+				free(trav_node);
+				pre_node->next = post_node;
+			}
+			else
+			{
+				result = -1;
+			}
 
+		}
 	}
 	else
 	{
