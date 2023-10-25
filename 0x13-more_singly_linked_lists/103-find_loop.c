b7478c9e6af842d7a21b78e41b0159d3241ea8e4
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-25 16:43:39 +0300

    Add 103-find_loo

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* find_listint_loop - detects loop in linkint_t linked list
+* @head: pointer to head node
+*
+* Return: pointer to loop node
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+	const listint_t *check_node = NULL;
+	const listint_t *h = head;
+
+	while (head != NULL)
+	{
+		check_node = h;
+		while (check_node != head)
+		{
+			if (head->next == check_node)
+			{
+				return (head->next);
+			}
+			check_node = check_node->next;
+		}
+		head = head->next;
+	}
+	return (NULL);
+}
