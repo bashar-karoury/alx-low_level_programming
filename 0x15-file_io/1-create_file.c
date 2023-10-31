7fb2955cbec2c30d8c615db438a2d075a6f5069e
bashar-karoury <basharalkaroury@hotmail.com>
2023-10-31 14:03:45 +0200

    Fix 1-creat_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,14 +17,14 @@ int create_file(const char *filename, char *text_content)
 	int i = 0;
 	int write_count = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (result);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (result);
 
-	while (text_content[i] != '\0')
+	while ((text_content != NULL) && (text_content[i] != '\0'))
 	{
 		write_count = write(fd, &(text_content[i]), 1);
 		if (write_count != 1)
