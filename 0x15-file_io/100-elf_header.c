0e3a622d66c2b040d5211582f19f8f66c865efaf
bashar-karoury <basharalkaroury@hotmail.com>
2023-11-03 15:42:45 +0200

    Fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -244,7 +244,7 @@ void  print_os_abi(Elf32_Ehdr *header)
 			printf("  OS/ABI:                            Stand-alone (embedded)\n");
 			break;
 		default:
-			printf("  OS/ABI:                            <unknown: 53>\n");
+			printf("  OS/ABI:                            <unknown: %c>\n", os);
 			break;
 	}
 }
