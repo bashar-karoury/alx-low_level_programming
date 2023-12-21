ab22a4c748f2e51f53c6d49a16c255b6c19d5760
bashar-karoury <basharalkaroury@hotmail.com>
2023-12-21 19:44:21 +0200

    Add task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ *
+ * Return: pointer to the newly created hash table, NULL if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht = NULL;
+	hash_node_t **arr = NULL;
+
+	new_ht = malloc(sizeof(hash_table_t));
+	if (new_ht == NULL)
+		return (NULL);
+	new_ht->size = size;
+	arr = malloc(sizeof(hash_table_t *) * size);
+	if (arr == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
+	new_ht->array = arr;
+	return (new_ht);
+
+}
