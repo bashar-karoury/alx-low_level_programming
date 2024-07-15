3961a71f7623c28267d533b9f1a4d5bdb471f3d3
bashar-karoury <basharalkaroury@hotmail.com>
2024-07-15 15:22:45 +0400

    Add

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,4 +2,6 @@
 #define _SERACH_ALGOS_H_
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int start, int end);
 #endif
